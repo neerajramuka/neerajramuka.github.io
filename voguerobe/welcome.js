74f82327555fb6e624e9460c7b5f8e858cfd1ec3
Neeraj Ramuka <neeraj@vinsol.com>
2016-02-12 21:21:56 +0530

    timer removed

--- voguerobe/welcome.js
+++ voguerobe/welcome.js
@@ -4,38 +4,38 @@ var isAndroid = /(android)/i.test(navigator.userAgent);
 
 $(document).ready(function() {
   // Set dates.
-  var futureDate  = new Date("February 14, 2016 12:00 AM UTC+05:30");
-  var currentDate = new Date();
+  // var futureDate  = new Date("February 14, 2016 12:00 AM UTC+05:30");
+  // var currentDate = new Date();
 
   // Calculate the difference in seconds between the future and current date
-  var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+  // var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
   // Calculate day difference and apply class to .clock for extra digit styling.
-  function dayDiff(first, second) {
-    return (second-first)/(1000*60*60*24);
-  }
+  // function dayDiff(first, second) {
+  //   return (second-first)/(1000*60*60*24);
+  // }
 
-  if (dayDiff(currentDate, futureDate) < 100) {
-    $('.clock').addClass('twoDayDigits');
-  } else {
-    $('.clock').addClass('threeDayDigits');
-  }
+  // if (dayDiff(currentDate, futureDate) < 100) {
+  //   $('.clock').addClass('twoDayDigits');
+  // } else {
+  //   $('.clock').addClass('threeDayDigits');
+  // }
 
-  if(diff < 0) {
-    diff = 0;
-  }
+  // if(diff < 0) {
+  //   diff = 0;
+  // }
 
   // Instantiate a coutdown FlipClock
-  clock = $('.clock').FlipClock(diff, {
-    clockFace: 'DailyCounter',
-    countdown: true
-  });
+  // clock = $('.clock').FlipClock(diff, {
+  //   clockFace: 'DailyCounter',
+  //   countdown: true
+  // });
 
   //Set zoom to make clock responsive
-  if(isMobile) {
-    var zoom = $(window).width()/620*0.75;
-    $('.clock').css('zoom', zoom);
-  };
+  // if(isMobile) {
+  //   var zoom = $(window).width()/620*0.75;
+  //   $('.clock').css('zoom', zoom);
+  // };
   if (isAndroid) {
     $( "#emailVal" ).focus(function() {
       var $target = $('html,body'); 
